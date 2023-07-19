1af3ea716028406af730cb4641ee9ad0cbb729c9
gdknas <mckevitt.r@gmail.com>
2023-07-18 23:58:04 -0500

    removed target directory

--- target/debug/deps/port_numbers_quiz-db18dfcfffacf8db.d
+++
@@ -1,7 +0,0 @@
-/Users/ryanmck/Documents/Code/Port Numbers Quiz/target/debug/deps/port_numbers_quiz-db18dfcfffacf8db.rmeta: src/main.rs src/getpairs.rs src/printlist.rs
-
-/Users/ryanmck/Documents/Code/Port Numbers Quiz/target/debug/deps/port_numbers_quiz-db18dfcfffacf8db.d: src/main.rs src/getpairs.rs src/printlist.rs
-
-src/main.rs:
-src/getpairs.rs:
-src/printlist.rs:
